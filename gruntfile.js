0edac6bde8e50bc98194f63f9efb06a72fa88685
Mikus Riekstins <mikus.riekstins@gmail.com>
2015-03-20 08:53:41 +0200

    More grunt tasks

--- gruntfile.js
+++ gruntfile.js
@@ -1,7 +1,33 @@
 
 module.exports = function(grunt) {
-  // Default tasks.
-  grunt.registerTask('default', function() {
-    grunt.log.write('Logging...').ok(); 
+  grunt.initConfig({
+    sass: {
+      options: {
+        style: 'compressed'
+      },
+      dist: {
+        files: {
+          'assets/css/main.css': 'assets/sass/main.scss'
+        }
+      }
+    },
+
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass']
+      },
+
+      livereload: {
+        options: { livereload: true },
+        files: ['assets/css/*.css', 'assets/js/*.js', '*.html']
+      }
+    }
   });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default tasks.
+  grunt.registerTask('default', ['sass', 'watch']); 
 };
